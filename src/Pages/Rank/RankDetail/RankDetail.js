e973e9457aad85bbfae4b41503401dfa2f24a361
jeongsooyoung <code.ameba@gmail.com>
2020-07-12 18:36:16 +0900

    Add: params.id와 user.rank 매칭하여 클릭한 유저 디테일 페이지 이동 및 유저 정보 가져오기 성공

--- src/Pages/Rank/RankDetail/RankDetail.js
+++ src/Pages/Rank/RankDetail/RankDetail.js
@@ -1,5 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, Fragment } from 'react';
+import { useParams } from 'react-router';
 import styled, { keyframes, css } from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faUsers } from '@fortawesome/free-solid-svg-icons';
 
 import Navigation from '../../../Components/Navigation/Navigation';
 import Footer from '../../../Components/Footer/Footer';
@@ -9,41 +12,67 @@ import { rankInfo } from '../../../config.js';
 
 const RankDetail = () => {
 	const [ speedMode, setSpeedMode ] = useState(2);
-
-	useEffect(() => {
-		console.log();
-	}, []);
+	const [ playMode, setPlayMode ] = useState(1);
+	const { id } = useParams();
+	const userInfo = rankInfo.filter(user => user.rank === Number(id));
 
 	const defaultUserImg =
 		'https://s3-ap-northeast-1.amazonaws.com/solution-userstats/metadata/character/42c729e64e31aea803e4881432f7b95129ce97535c29e4f9a72919a9f267b418.png';
 
 	const speedModeText = [ '빠름', '매우빠름', '무한부스터' ];
 
+	console.log(userInfo);
+
 	return (
-		<RankDetailContainer>
-			<NameCard />
-			<RainbowBg />
-			<section>
-				<CircleChartWrapper />
-				<CurrentFiftyGames />
-				<CommentWrapper />
-			</section>
-			<div className='speedModeBtnWrapper'>
-				{speedModeText.map((mode, idx) => (
-					<SpeedModeBtn
-						speedMode={speedMode === idx + 1}
-						onClick={() => setSpeedMode(idx + 1)}
-						key={mode + idx}
-					>
-						{mode}
-					</SpeedModeBtn>
-				))}
-			</div>
-		</RankDetailContainer>
+		<Fragment>
+			<Navigation />
+			<RankDetailContainer>
+				<NameCard>
+					<div className='flagBg' />
+					{userInfo.map(user => (
+						<div className='nameCardWrapper' key={user.rank + user.name}>
+							<img src={user.img || defaultUserImg} alt={user.name} />
+							<div className='infoWrapper'>
+								<h2>{user.name}</h2>
+							</div>
+						</div>
+					))}
+					<div className='playModeBtnWrapper'>
+						<IndividualPlayModeBtn playMode={playMode === 1} onClick={() => setPlayMode(1)}>
+							<FontAwesomeIcon className='userIcon' icon={faUser} />
+							개인전
+						</IndividualPlayModeBtn>
+						<TeamPlayModeBtn playMode={playMode === 2} onClick={() => setPlayMode(2)}>
+							<FontAwesomeIcon className='usersIcon' icon={faUsers} />
+							팀전
+						</TeamPlayModeBtn>
+					</div>
+				</NameCard>
+				<RainbowBg />
+				<section>
+					<CircleChartWrapper />
+					<CurrentFiftyGames />
+					<CommentWrapper />
+				</section>
+				<div className='speedModeBtnWrapper'>
+					{speedModeText.map((mode, idx) => (
+						<SpeedModeBtn
+							speedMode={speedMode === idx + 1}
+							onClick={() => setSpeedMode(idx + 1)}
+							key={mode + idx}
+						>
+							{mode}
+						</SpeedModeBtn>
+					))}
+				</div>
+			</RankDetailContainer>
+			<Footer />
+		</Fragment>
 	);
 };
 
 const primaryColor = '#0177fe';
+const deepPrimaryColor = '#015ecc';
 
 const RankDetailContainer = styled.main`
 	width: 100%;
@@ -53,6 +82,7 @@ const RankDetailContainer = styled.main`
 	justify-content: center;
 	align-items: center;
 	flex-direction: column;
+	padding-top: 50px;
 
 	& > section {
 		width: 70%;
@@ -63,7 +93,6 @@ const RankDetailContainer = styled.main`
 		div {
 			width: 325px;
 			height: 265px;
-			background-color: #fff;
 			margin-bottom: 10px;
 		}
 	}
@@ -82,9 +111,97 @@ const RankDetailContainer = styled.main`
 const NameCard = styled.section`
 	width: 70%;
 	height: 180px;
-	background-color: #fff;
 	border-left: 4px solid ${primaryColor};
 	margin-bottom: 25px;
+	position: relative;
+
+	.nameCardWrapper {
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		display: flex;
+		justify-content: flex-start;
+		align-items: center;
+
+		img {
+			width: 180px;
+			height: auto;
+		}
+
+		.infoWrapper {
+			height: 100%;
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			z-index: 2;
+
+			h2 {
+				font-size: 2.5rem;
+				font-weight: 600;
+				padding-bottom: 50px;
+			}
+		}
+	}
+
+	.playModeBtnWrapper {
+		position: absolute;
+		display: flex;
+		left: 240px;
+		top: 110px;
+		z-index: 2;
+	}
+
+	.flagBg {
+		background-image: url('https://ak.picdn.net/shutterstock/videos/768526/thumb/1.jpg');
+		background-size: cover;
+		opacity: 0.03;
+		width: 100%;
+		height: 100%;
+		position: absolute;
+		top: 0;
+		z-index: 1;
+	}
+`;
+
+const IndividualPlayModeBtn = styled.button`
+	width: 104px;
+	height: 25px;
+	border: 1px solid ${deepPrimaryColor};
+	font-size: 0.75rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	outline: none;
+	cursor: pointer;
+	border-radius: 5px 0 0 5px;
+	color: ${props => (props.playMode ? '#fff' : deepPrimaryColor)};
+	background-color: ${props => (props.playMode ? deepPrimaryColor : '#fff')};
+
+	.userIcon {
+		margin-right: 10px;
+		font-size: 0.9rem;
+	}
+`;
+
+const TeamPlayModeBtn = styled.button`
+	width: 104px;
+	height: 25px;
+	border: 1px solid ${deepPrimaryColor};
+	font-size: 0.75rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	outline: none;
+	cursor: pointer;
+	border-radius: 0 5px 5px 0;
+	color: ${props => (props.playMode ? '#fff' : deepPrimaryColor)};
+	background-color: ${props => (props.playMode ? deepPrimaryColor : '#fff')};
+
+	.usersIcon {
+		margin-right: 10px;
+		font-size: 1rem;
+	}
 `;
 
 const RainbowBgAnimation = keyframes`
@@ -108,11 +225,14 @@ const RainbowBg = styled.div`
 	margin-bottom: 25px;
 `;
 
-const CircleChartWrapper = styled.div``;
+const CircleChartWrapper = styled.div`background-color: #fff;`;
 
-const CurrentFiftyGames = styled.div`margin: 0 10px;`;
+const CurrentFiftyGames = styled.div`
+	margin: 0 10px;
+	background-color: #fff;
+`;
 
-const CommentWrapper = styled.div``;
+const CommentWrapper = styled.div`background-color: #fff;`;
 
 const SpeedModeBtn = styled.div`
 	width: 90px;
--- src/Pages/Rank/RankPageInfo/RankPageInfo.js
+++ src/Pages/Rank/RankPageInfo/RankPageInfo.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser, faUsers } from '@fortawesome/free-solid-svg-icons';
 
